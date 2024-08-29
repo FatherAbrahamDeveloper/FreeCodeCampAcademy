71dba44fd38ec1a6d64c21d4f2a7f02ebfc58465
oayodele <oayodele@xplugng.com>
2024-08-29 20:48:51 +0100

    Added many CSS and static files

---
+++ Using.cs
@@ -0,0 +1,3 @@
+﻿global using XP.Kit.Caching;
+global using XP.Kit.CommonUItils.Enums;
+global using XP.Kit.CommonUItils;
---
+++ wwwroot/js/pages/main/registration/reg-wizard.js
@@ -0,0 +1,39 @@
+﻿$("#photoUpload").on('change', (function (e) {
+    return doPhotoUpload();
+}));
+function doPhotoUpload() {
+
+    var fileInput = document.getElementById('photoUpload');
+    var file = fileInput.files[0];
+    var formData = new FormData();
+    formData.append('photoUpload', file);
+
+    $.ajax({
+        url: '/register/upload-photo',
+        type: "POST",
+        data: formData,
+        contentType: false,
+        cache: false,
+        processData: false,
+        success: function (data) {
+            unblockMainView();
+            if (!data.IsAuthenticated) {
+                //ErrorToast("Upload Error! Try again", "dvError");
+                return;
+            }
+            if (!data.IsSuccessful) {
+                var retError = !isEmpty(data.Error) ? data.Error : "Unknown error occured. Please try again later!";
+                //ErrorToast(retError, "dvError");
+                alert(retError);
+                return;
+            } else {
+                $("#photoView").attr('src', '/AppDocs/TempUploads/' + data.PhotoPath);
+                $("#frmPicUpload")[0].reset();
+                return false;
+            }
+        },
+        error: function (e) {
+            $("#err").html(e).fadeIn();
+        }
+    });
+}
