e764ba50822c8996af9111bbca4cc68269545751
oayodele <oayodele@xplugng.com>
2024-09-03 16:01:28 +0100

    Completed Program.cs

--- Program.cs
+++ Program.cs
@@ -35,7 +35,6 @@ builder.Services.AddSingleton<IStartSession, InitSession>();
 builder.Services.AddSingleton<ISessionStore, DistributedSessionStoreWithStart>();
 
 
-
 //Add services to the container
 var app = builder.Build();
 
@@ -46,7 +45,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     //app.UseHsts();
 }
-
 
 app.UseStaticFiles();
 app.UseRouting();
